bc3212cfbcbe61e22b1f0d55ac45687b8a844e23
smile77777 <1153588013@qq.com>
2023-03-16 09:35:04 +0800

    second

--- boot-01/src/main/java/com/ljc/boot/controller/helloController.java
+++ boot-01/src/main/java/com/ljc/boot/controller/helloController.java
@@ -1,10 +1,20 @@
 package com.ljc.boot.controller;
 
+import com.ljc.boot.bean.Car;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class helloController {
+
+    @Autowired
+    Car car;
+
+    @RequestMapping("/mycar")
+    public Car mycar(){
+        return car;
+    }
 
     @RequestMapping("/hello")
     public String handle(){
